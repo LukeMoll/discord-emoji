2764fe26302d704746dff8ea3e4a3e09c57218cc
Luke Moll <lukemoll@users.noreply.github.com>
2023-04-28 22:26:19 +0100

    Bot and API running at the same time

--- discordemoji/__main__.py
+++ discordemoji/__main__.py
@@ -2,9 +2,18 @@ from .DiscordClient import get_client
 from .api import get_app
 from .config import DISCORD_BOT_TOKEN
 
+import threading
+
 if __name__ == "__main__":
     client = get_client()
-    client.run(DISCORD_BOT_TOKEN) # TODO: need to run this "in the background"
-
     app = get_app()
-    app.run()
+
+    t1 = threading.Thread(target=client.run, args=(DISCORD_BOT_TOKEN,))
+    t2 = threading.Thread(target=app.run)
+
+    t1.start()
+    t2.start()
+
+    # So as not to immediately exit the main thread
+    t1.join()
+    t2.join()
